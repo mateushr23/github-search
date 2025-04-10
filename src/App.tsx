b920bc2becb00f91ddcd17c7128c1e68b0df3f8c
mateushr23 <mateushr23@gmail.com>
2025-04-10 13:24:36 -0300

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { MagnifyingGlass } from "@phosphor-icons/react"
+import git from "../assets/git.png"
+import dots from "../assets/dots.png"
+import ellipse1 from "../assets/ellipse1.png"
+import ellipse2 from "../assets/ellipse2.png"
 import { useState } from "react"
 import axios from "axios"
 
@@ -33,25 +37,26 @@ export default function App() {
   }
 
   return (
-    <div className="min-h-screen bg-[#000000] relative overflow-hidden">
-      <div className="absolute top-0 right-0 w-1/4 h-full bg-gradient-to-l from-[#005cff]/40 to-transparent" />
-      <div className="absolute bottom-0 left-0 w-1/4 h-full bg-gradient-to-r from-[#005cff]/40 to-transparent" />
-      <div className="absolute inset-0 opacity-10 pointer-events-none">
-        {Array.from({ length: 20 }).map((_, rowIndex) => (
-          <div key={rowIndex} className="flex">
-            {Array.from({ length: 30 }).map((_, colIndex) => (
-              <div
-                key={colIndex}
-                className="w-2 h-2 m-2 rounded-full bg-[#ffffff]"
-              />
-            ))}
-          </div>
-        ))}
-      </div>
-
+    <div className="min-h-screen bg-[#1F1F1F] relative overflow-hidden">
+      <img
+        src={dots}
+        alt="Decorative dots"
+        className="absolute top-7 left-18 w-[240px] h-[240px]"
+      />
+      <img
+        src={ellipse1}
+        alt="Decorative ellipse"
+        className="absolute top-0 right-0 w-[900px] h-[900px]"
+      />
+      <img
+        src={ellipse2}
+        alt="Decorative ellipse"
+        className="absolute bottom-0 left-0 w-[700px] h-[700px]"
+      />
       <div className="container mx-auto px-4 py-12 relative z-10 flex items-center justify-center min-h-screen">
-        <div className="bg-black px-80 py-60 rounded-lg shadow-lg">
-          <div className="flex justify-center items-center mb-8">
+        <div className="bg-black w-[1200px] h-[540px] flex flex-col items-center justify-center rounded-lg shadow-lg">
+          <div className="flex justify-center items-center mb-8 gap-3">
+            <img src={git} alt="" />
             <h1 className="text-[#ffffff] text-4xl font-bold">Perfil GitHub</h1>
           </div>
 
@@ -84,7 +89,7 @@ export default function App() {
             <div className="max-w-3xl mx-auto bg-[#dddddd] rounded-lg p-6 flex flex-col gap-6 items-center">
               {error && <p className="text-red-500 text-center">{error}</p>}
               {userData && (
-                <div className="flex flex-row gap-6 items-start justify-center">
+                <div className="flex flex-row gap-6 items-center justify-center">
                   <img
                     src={userData.avatar_url}
                     alt={`${userData.name}'s avatar`}
